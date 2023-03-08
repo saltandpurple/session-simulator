3dbdf26f8b950f58f7ebd62c59a36c93263e9fad
saltandpurple <d0ghouse@freenet.de>
2023-03-08 16:03:03 +0100

    added ability to get + adjust active sessions

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from http import cookies
 import socket
-import random
+
 
 # todo: get /activesessions that returns (mock) number of active sessions
 # todo: post /activesessions that sets the (mock) number of active sessions
-
 class Server(BaseHTTPRequestHandler):
+    activeSessions = 5
+
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -18,13 +18,16 @@ class Server(BaseHTTPRequestHandler):
         self._set_response()
         if self.path == "/":
             self.wfile.write("You're on machine: {}\n Your cookies: {}".format(socket.gethostname(), self.headers.get('Cookie')).encode('utf-8'))
+        elif self.path == "/activesessions":
+            self.wfile.write("{}".format(Server.activeSessions).encode('utf-8'))
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
-
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        if self.path == "/activesessions":
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            Server.activeSessions = post_data.decode('utf-8')
+            self._set_response()
+            self.wfile.write("Number of active sessions is now {}".format(self.activeSessions).encode('utf-8'))
 
 
 def run(server_class=HTTPServer, handler_class=Server, port=8080):
